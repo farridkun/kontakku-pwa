63fe8725c62c6c5bd291346dc25d343b35ac577f
farridkun <farridkuntoro@mahasiswa.unikom.ac.id>
2021-02-28 22:19:21 +0700

    Adding caching with static and dynamic

--- sw.js
+++ sw.js
@@ -1,13 +1,38 @@
+const staticCaches = 'static-cache'
+const dynamicCache = 'dynamic-cache'
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/materialize.min.css',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
+]
+
 //install proses
 self.addEventListener('install', e => {
-    console.log('sw telah berhasil diinstall');
+    e.waitUntil(
+        caches.open(staticCaches).then(cache => {
+            cache.addAll(assets)
+        })
+    )
 })
 
 //activate
 self.addEventListener('activate', e=> {
-    console.log('sw telah aktif');
+    console.log('activate');
 })
 
 self.addEventListener('fetch', e => {
-    console.log('sw fetch event', e);
+    e.respondWith(
+        caches.match(e.request).then(staticRes => {
+            return staticRes || fetch(e.request).then(dynamicRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(e.request.url, dynamicRes.clone())
+                    return dynamicRes
+                })
+            })
+        })
+    )
 })
