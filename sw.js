4fabd1ab1aa047d27c49980765f0a43e9f1187a6
farridkun <farridkuntoro@mahasiswa.unikom.ac.id>
2021-02-28 21:31:22 +0700

    activate ServiceWorker efficiently

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
 //install proses
-self.addEventListener('install',e=>{
+self.addEventListener('install', e => {
     console.log('sw telah berhasil diinstall');
+})
+
+//activate
+self.addEventListener('activate', e=> {
+    console.log('sw telah aktif');
 })
